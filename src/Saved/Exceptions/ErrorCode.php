7b4c5106994ba2b0733460d2aad29d5645b38e57
joanna.chen <joanna.chen@104.com.tw>
2022-08-29 13:45:25 +0800

    fix const

--- src/Saved/Exceptions/ErrorCode.php
+++ src/Saved/Exceptions/ErrorCode.php
@@ -6,6 +6,6 @@ namespace Corp104\Jbc\Saved\Exceptions;
 
 class ErrorCode
 {
-    const MSG_SAVE_JOB_EXCEED_LIMIT_ERROR = '儲存工作數量超過 200 筆';
-    const CODE_SAVE_JOB_EXCEED_LIMIT_ERROR = 37001;
+    public const MSG_SAVE_JOB_EXCEED_LIMIT_ERROR = '儲存工作數量超過 200 筆';
+    public const CODE_SAVE_JOB_EXCEED_LIMIT_ERROR = 37001;
 }
